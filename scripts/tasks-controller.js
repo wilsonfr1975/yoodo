8027314df1ac811ab3ee84485c5bade2b9a1e0fc
Lauritz Hilsøe <mail@lauritz.me>
2015-01-23 00:31:38 +0100

    Move JS logic to new module and jQuery plugin

---
+++ scripts/tasks-controller.js
@@ -0,0 +1,36 @@
+tasksController = function () {
+  var taskPage;
+  var initialized = false;
+  return {
+    init: function(page) {
+      if (!initialized) {
+        taskPage = page;
+
+        $(taskPage).find('[required]').prev('label').append('<span>*</span>').children('span').addClass('required');
+        $(taskPage).find('tbody tr:even').addClass('even');
+
+        $(taskPage).find('#btnAddTask').click(function(event) {
+          event.preventDefault();
+          $(taskPage).find('#taskCreation').removeClass('not');
+        });
+
+        $(taskPage).find('tbody tr').click(function(event) {
+          $(event.target).closest('td').siblings().andSelf().toggleClass('rowHighlight');
+        });
+
+        $(taskPage).find('#tblTasks tbody').on('click', '.deleteRow', function(event) {
+          event.preventDefault();
+          $(event.target).parents('tr').remove();
+        });
+
+        $(taskPage).find('#saveTask').click(function(event) {
+          event.preventDefault();
+          var task = $('form').toObject();
+          $('#taskRow').tmpl(task).appendTo($(taskPage).find('#tblTasks tbody'));
+        });
+
+        initialized = true;
+      }
+    }
+  }
+}();
